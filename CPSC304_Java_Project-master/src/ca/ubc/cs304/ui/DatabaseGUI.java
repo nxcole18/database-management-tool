d17d832f1e40cae377de274137bebfbb451892b9
nxcolel <nxcolel@student.ubc.ca>
2023-11-21 01:34:37 -0800

    adding the starter code for Java/Oracle project (as provided by CPSC 304) with minor edits. in the process of creating the GUI and replacing old code.

---
+++ CPSC304_Java_Project-master/src/ca/ubc/cs304/ui/DatabaseGUI.java
@@ -0,0 +1,11 @@
+package ca.ubc.cs304.ui;
+
+public class DatabaseGUI {
+    public DatabaseGUI() {
+        new LandingScreen();
+    }
+
+    public static void main(String[] args) {
+        new DatabaseGUI();
+    }
+}
---
+++ CPSC304_Java_Project-master/src/ca/ubc/cs304/ui/InsertTable.java
@@ -0,0 +1,17 @@
+package ca.ubc.cs304.ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class InsertTable extends JFrame implements ActionListener {
+
+    public InsertTable() {
+        super("Insert Page");
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
