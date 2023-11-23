f0769efbdfefce3eb33a91ee6072db5cdb06d743
UBC Student <wwu947@students.cs.ubc.ca>
2023-11-23 00:39:18 -0800

    Update INSERTS.sql

--- INSERTS.sql
+++ INSERTS.sql
@@ -1,1 +1,33 @@
-INSERT INTO TEAM 
+CREATE TABLE Team1(
+	Organization VARCHAR(50) PRIMARY KEY,
+	Country VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Team2(
+	ID INT PRIMARY KEY,
+	Name VARCHAR(50) UNIQUE NOT NULL,
+	Organization VARCHAR(50) NOT NULL,
+	FOREIGN KEY (Organization) REFERENCES Team1(Organization)
+        ON DELETE CASCADE
+);
+
+INSERT INTO Team1(Organization, Country)
+VALUES
+  ('KSV GEN', 'SK'),
+  ('SK Telecom', 'SK'),
+  ('KT Corp', 'SK'),
+  ('Kia', 'SK'),
+  ('JingDong', 'CN'),
+  ('Bilibili', 'CN'),
+  ('LiNing', 'CN'),
+  ('Weibo', 'CN'),
+  ('NR', 'NA'), 
+  ('C9', 'NA'),
+  ('Liquid', 'NA'),
+  ('MAD Lions', 'EU'),
+  ('Gamers2', 'EU'),
+  ('Fnatic', 'EU'),
+  ('BDS Gaming', 'EU'),
+  ('Giga', 'VN');
+
+
