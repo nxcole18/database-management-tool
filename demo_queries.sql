18e7736af475664a93a2bc17c78f930a5f783079
Ezepheros <ethanzhao9876@gmail.com>
2023-11-26 14:41:36 -0800

    added demo queries

---
+++ demo_queries.sql
@@ -0,0 +1,18 @@
+-- insert
+INSERT INTO Player VALUES (998, 'Jack', 'Zhen', 'Canada', DATE '2023-09-05', 10300, 101);
+
+-- update
+
+-- delete
+
+-- select (relational algebra select NOT SELECT statement)
+
+-- project
+
+-- join
+
+
+-- aggregation group by
+-- having aggregation
+-- nested aggregation group by
+-- division
