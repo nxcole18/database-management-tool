ef1a9dcbb1219265b9bbaad1fc5b553a4ae56cc1
UBC Student <wwu947@students.cs.ubc.ca>
2023-11-29 14:14:28 -0800

    Update INSERTS.sql

--- INSERTS.sql
+++ INSERTS.sql
@@ -3,39 +3,22 @@ VALUES
 	('KSV GEN', 'SK'),
 	('SK Telecom', 'SK')
 	('KT Corp', 'SK'),
-	('Kia', 'SK'),
 	('JingDong', 'CN'),
 	('Bilibili', 'CN'),
 	('LiNing', 'CN'),
 	('Weibo', 'CN'),
-	('NR', 'NA'), 
-	('Cloud9', 'NA'),
-	('Liquid', 'NA'),
-	('MAD Lions', 'EU'),
-	('Gamers2', 'EU'),
-	('Fnatic', 'EU'),
-	('BDS Gaming', 'EU'),
-	('Giga', 'VN');
-
+	('NR', 'NA');
 
 INSERT INTO Team2(ID, Name, Organization)
 VALUES
 	(01, 'Gen.G', 'KSV GEN'),
 	(02, 'T1', 'SK Telecom'),
 	(03, 'KT', 'KT Corp'),
-	(04, 'DK', 'Kia'),
-	(05, 'JDG', 'JingDong'),
-	(06, 'BLG', 'Bilibili'),
-	(07, 'LNG', 'LiNing'),
-	(08, 'WBG', 'Weibo'),
-	(09, 'NRG', 'NR'),
-	(10, 'C9', 'Cloud9'),
-	(11, 'TL', 'Liquid'),
-	(12, 'MAD', 'MAD Lions'),
-	(13, 'G2', 'Gamers2'),
-	(14, 'FNC', 'Fnatic'),
-	(15, 'BDS', 'BDS Gaming'),
-	(16, 'GAM', 'Giga');
+	(04, 'JDG', 'JingDong'),
+	(05, 'BLG', 'Bilibili'),
+	(06, 'LNG', 'LiNing'),
+	(07, 'WBG', 'Weibo'),
+	(08, 'NRG', 'NR');
 
 CREATE TABLE PLAYER(
 	ID INT PRIMARY KEY,
@@ -50,4 +33,18 @@ FOREIGN KEY (Team_ID) REFERENCES Team(ID) ON DELETE CASCADE ON UPDATE CASCADE
 
 INSERT INTO Player(ID, First_name, Last_name, Country, Join_date, Ranking, Team_ID)
 VALUES
-	(),
+	(11, 'Doran', ),
+	(12, 'Peanut', 'Han', 'SK', '2022-01-01', 12, 01),
+	(13, 'Chovy', 'Jeong', 'SK', '2022-01-01', 13, 01), 
+	(14, 'Peyz', ''),
+	(15, 'Delight', ''),
+	(21, 'Zeus', 'Choi', 'SK', '2020-11-05', 21, 02),
+	(22, 'Oner', ''),
+	(23, 'Faker', 'Lee', 'SK', '2013-01-01', 23, 02),
+	(24, 'Gumayusi', 'Lee', 'SK', '2021-02-03', 24, 02),
+	
+	 
+	 
+	 
+	 
+	 
