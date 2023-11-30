72bef6ea2196b7d0c6188c10543031c022416366
UBC Student <wwu947@students.cs.ubc.ca>
2023-11-30 15:53:00 -0800

    Added inserts for division
    Edited for format consistency

--- ddl.sql
+++ ddl.sql
@@ -128,7 +128,7 @@ CREATE TABLE BusinessMember(
         ON DELETE CASCADE
 );
 
-CREATE TABLE PLAYER(
+CREATE TABLE Player(
 	ID INT PRIMARY KEY,
 	First_name VARCHAR(50) NOT NULL,
 	Last_name VARCHAR(50) NOT NULL,
@@ -165,18 +165,13 @@ INSERT INTO Team2		VALUES (103, 'TeamC', 'OrgC');
 INSERT INTO Team2		VALUES (104, 'TeamD', 'OrgD');
 INSERT INTO Team2		VALUES (105, 'TeamE', 'OrgE');
 
-INSERT INTO Tournament	VALUES ('TournamentA', DATE '2012-04-08', DATE '2012-04-12', 'Progression', 
-'VenueA', 'Shanghai', 11, 101);
-INSERT INTO Tournament	VALUES ('TournamentB', DATE '2020-12-07', DATE '2020-12-21', 'Elimination', 
-'VenueB', 'Vancouver', 55, 101);
-INSERT INTO Tournament	VALUES ('TournamentC', DATE '2016-11-03', DATE '2016-11-09', 'Elimination', 
-'VenueC', 'Helsinki', 22, 102);
-INSERT INTO Tournament	VALUES ('LOL', DATE '2018-11-05', DATE '2018-11-05', 'Elimination', 'VenueD', 
-'Incheon', 33,103);
-INSERT INTO Tournament	VALUES ('2018 Winter Olympics', DATE '2018-02-09', DATE '2018-02-25', 
-'Progression', 'VenueE', 'PyeongChang', 44, 104);
+INSERT INTO Tournament	VALUES ('TournamentA', DATE '2012-04-08', DATE '2012-04-12', 'Progression', 'VenueA', 'Shanghai', 11, 101);
+INSERT INTO Tournament	VALUES ('TournamentB', DATE '2020-12-07', DATE '2020-12-21', 'Elimination', 'VenueB', 'Vancouver', 55, 101);
+INSERT INTO Tournament	VALUES ('TournamentC', DATE '2016-11-03', DATE '2016-11-09', 'Elimination', 'VenueC', 'Helsinki', 22, 102);
+INSERT INTO Tournament	VALUES ('LOL', DATE '2018-11-05', DATE '2018-11-05', 'Elimination', 'VenueD', 'Incheon', 33,103);
+INSERT INTO Tournament	VALUES ('2018 Winter Olympics', DATE '2018-02-09', DATE '2018-02-25', 'Progression', 'VenueE', 'PyeongChang', 44, 104);
 
-INSERT INTO Match1		VALUES ('Finals', 5);
+INSERT INTO Match1		VALUES ('Final', 5);
 INSERT INTO Match1		VALUES ('Semifinals', 5);
 INSERT INTO Match1		VALUES ('Qualification', 3);
 INSERT INTO Match1		VALUES ('Quarterfinals', 3);
@@ -235,14 +230,18 @@ INSERT INTO Player		VALUES (1010, 'Tim', 'Thomas', 'Finland', DATE '2022-10-31',
 
 --new inserts for milestone5
 --based on LOL 2023 eliminiation rounds data
-INSERT INTO Team1 VALUES ('KSV GEN', 'SK');
-INSERT INTO Team1 VALUES ('SK Telecom', 'SK');
-INSERT INTO Team1 VALUES ('KT Corp', 'SK');
-INSERT INTO Team1 VALUES ('JingDong', 'CN');
-INSERT INTO Team1 VALUES ('Bilibili', 'CN');
-INSERT INTO Team1 VALUES ('LiNing', 'CN');
-INSERT INTO Team1 VALUES ('Weibo', 'CN');
-INSERT INTO Team1 VALUES ('NR', 'NA');
+INSERT INTO Venue VALUES ('Gocheok Sky Dome', 'Seoul', 'South Korea', 16744);
+
+INSERT INTO Tournament VALUES ('2023 LOL Worlds', DATE '2023-11-02', DATE '2023-11-19', 'Elimination', 'Gocheok Sky Dome', 'Seoul', 22, 02);
+
+INSERT INTO Team1 VALUES ('KSV GEN', 'South Korea');
+INSERT INTO Team1 VALUES ('SK Telecom', 'South Korea');
+INSERT INTO Team1 VALUES ('KT Corp', 'South Korea');
+INSERT INTO Team1 VALUES ('JingDong', 'China');
+INSERT INTO Team1 VALUES ('Bilibili', 'China');
+INSERT INTO Team1 VALUES ('LiNing', 'China');
+INSERT INTO Team1 VALUES ('Weibo', 'China');
+INSERT INTO Team1 VALUES ('NR', 'Canada');
 
 INSERT INTO Team2 VALUES (01, 'Gen.G', 'KSV GEN');
 INSERT INTO Team2 VALUES (02, 'T1', 'SK Telecom');
@@ -253,50 +252,46 @@ INSERT INTO Team2 VALUES (06, 'LNG', 'LiNing');
 INSERT INTO Team2 VALUES (07, 'WBG', 'Weibo');
 INSERT INTO Team2 VALUES (08, 'NRG', 'NR');
 
-INSERT INTO PLAYER VALUES (11, 'Doran', 'Choi', 'SK', DATE '2022-01-01', 11, 01);
-INSERT INTO PLAYER VALUES (12, 'Peanut', 'Han', 'SK', DATE '2022-01-01', 12, 01);
-INSERT INTO PLAYER VALUES (13, 'Chovy', 'Jeong', 'SK', DATE '2022-01-01', 13, 01); 
-INSERT INTO PLAYER VALUES (14, 'Peyz', 'Kim', 'SK', DATE '2023-01-01', 14, 01);
-INSERT INTO PLAYER VALUES (15, 'Delight', 'Yoo', 'SK', DATE '2023-01-01', 15, 01);
-INSERT INTO PLAYER VALUES (21, 'Zeus', 'Choi', 'SK', DATE '2020-11-05', 21, 02);
-INSERT INTO PLAYER VALUES (22, 'Oner', 'Mun', 'SK', DATE '2021-01-01', 22, 02);
-INSERT INTO PLAYER VALUES (23, 'Faker', 'Lee', 'SK', DATE '2013-01-01', 23, 02);
-INSERT INTO PLAYER VALUES (24, 'Gumayusi', 'Lee', 'SK', DATE '2021-02-03', 24, 02);
-INSERT INTO PLAYER VALUES (25, 'Keria', 'Ryu', 'SK', DATE '2020-11-05', 25, 02);
-INSERT INTO PLAYER VALUES (31, 'Kiin', 'Kim', 'SK', DATE '2023-01-01', 31, 03);
-INSERT INTO PLAYER VALUES (32, 'Cuzz', 'Moon', 'SK', DATE '2023-01-01', 32, 03);
-INSERT INTO PLAYER VALUES (33, 'Bdd', 'Gwak', 'SK', DATE '2023-01-02', 33, 03);
-INSERT INTO PLAYER VALUES (34, 'Aiming', 'Kim', 'SK', DATE '2023-01-01', 34, 03);
-INSERT INTO PLAYER VALUES (35, 'Lehends', 'Son', 'SK', DATE '2023-01-01', 35, 03);
-INSERT INTO PLAYER VALUES (41, '369', 'Bai', 'CN', DATE '2023-01-01', 41, 04);
-INSERT INTO PLAYER VALUES (42, 'Kanavi', 'Seo', 'SK', DATE '2022-01-01', 42, 04);
-INSERT INTO PLAYER VALUES (43, 'Knight', 'Zhuo', 'CN', DATE '2023-01-01', 43, 04);
-INSERT INTO PLAYER VALUES (44, 'Ruler', 'Park', 'SK', DATE '2023-01-01', 44, 04);
-INSERT INTO PLAYER VALUES (45, 'Missing', 'Lou', 'CN', DATE '2023-01-01', 45, 04);
-INSERT INTO PLAYER VALUES (51, 'Bin', 'Chen', 'CN', DATE '2021-01-01', 51, 05);
-INSERT INTO PLAYER VALUES (52, 'Xun', 'Peng', 'CN', DATE '2023-01-01', 52, 05);
-INSERT INTO PLAYER VALUES (53, 'Yagao', 'Zeng', 'CN', DATE '2022-01-01', 53, 05);
-INSERT INTO PLAYER VALUES (54, 'Elk', 'Zhao', 'CN', DATE '2023-02-01', 54, 05);
-INSERT INTO PLAYER VALUES (55, 'On', 'Luo', 'CN', DATE '2023-02-02', 55, 05);
-INSERT INTO PLAYER VALUES (61, 'Zika', 'Tang', 'CN', DATE '2023-01-01', 61, 06);
-INSERT INTO PLAYER VALUES (62, 'Tarzan', 'Lee', 'SK', DATE '2023-01-01', 62, 06);
-INSERT INTO PLAYER VALUES (63, 'Scout', 'Lee', 'SK', DATE '2023-01-01', 63, 06);
-INSERT INTO PLAYER VALUES (64, 'LP', 'Li', 'CN', DATE '2022-01-01', 64, 06);
-INSERT INTO PLAYER VALUES (65, 'Hang', 'Fu', 'CN', DATE '2023-01-01', 65, 06);
-INSERT INTO PLAYER VALUES (71, 'TheShy', 'Kang', 'SK', DATE '2022-01-01', 71, 07);
-INSERT INTO PLAYER VALUES (72, 'Karsa', 'Hung', 'PCS', DATE '2023-01-01', 72, 07);
-INSERT INTO PLAYER VALUES (73, 'Xiaohu', 'Li', 'CN', DATE '2023-01-01', 73, 07);
-INSERT INTO PLAYER VALUES (74, 'Light', 'Wang', 'CN', DATE '2023-01-01', 74, 07);
-INSERT INTO PLAYER VALUES (75, 'Crisp', 'Liu', 'CN', DATE '2020-01-01', 75, 07);
-INSERT INTO PLAYER VALUES (81, 'Dhokla', 'Doshi', 'USA', DATE '2023-04-06', 81, 08);
-INSERT INTO PLAYER VALUES (82, 'Contractz', 'Garcia', 'USA', DATE '2023-04-06', 82, 08);
-INSERT INTO PLAYER VALUES (83, 'Palafox', 'Palafox', 'USA', DATE '2023-04-06', 83, 08);
-INSERT INTO PLAYER VALUES (84, 'FBI', 'Huang', 'AUS', DATE '2023-05-27', 84, 08);
-INSERT INTO PLAYER VALUES (85, 'IgNar', 'Lee', 'SK', DATE '2023-05-27', 85, 08);
-
-INSERT INTO Venue VALUES ('Gocheok Sky Dome', 'Seoul', 'South Korea', 16744);
-
-INSERT INTO Tournament VALUES ('2023 LOL Worlds', DATE '2023-11-02', DATE '2023-11-19', 'Elimination', 'Gocheok Sky Dome', 'Seoul', 22, 02);
+INSERT INTO Player VALUES (11, 'Doran', 'Choi', 'South Korea', DATE '2022-01-01', 11, 01);
+INSERT INTO Player VALUES (12, 'Peanut', 'Han', 'South Korea', DATE '2022-01-01', 12, 01);
+INSERT INTO Player VALUES (13, 'Chovy', 'Jeong', 'South Korea', DATE '2022-01-01', 13, 01); 
+INSERT INTO Player VALUES (14, 'Peyz', 'Kim', 'South Korea', DATE '2023-01-01', 14, 01);
+INSERT INTO Player VALUES (15, 'Delight', 'Yoo', 'South Korea', DATE '2023-01-01', 15, 01);
+INSERT INTO Player VALUES (21, 'Zeus', 'Choi', 'South Korea', DATE '2020-11-05', 21, 02);
+INSERT INTO Player VALUES (22, 'Oner', 'Mun', 'South Korea', DATE '2021-01-01', 22, 02);
+INSERT INTO Player VALUES (23, 'Faker', 'Lee', 'South Korea', DATE '2013-01-01', 23, 02);
+INSERT INTO Player VALUES (24, 'Gumayusi', 'Lee', 'South Korea', DATE '2021-02-03', 24, 02);
+INSERT INTO Player VALUES (25, 'Keria', 'Ryu', 'South Korea', DATE '2020-11-05', 25, 02);
+INSERT INTO Player VALUES (31, 'Kiin', 'Kim', 'South Korea', DATE '2023-01-01', 31, 03);
+INSERT INTO Player VALUES (32, 'Cuzz', 'Moon', 'South Korea', DATE '2023-01-01', 32, 03);
+INSERT INTO Player VALUES (33, 'Bdd', 'Gwak', 'South Korea', DATE '2023-01-02', 33, 03);
+INSERT INTO Player VALUES (34, 'Aiming', 'Kim', 'South Korea', DATE '2023-01-01', 34, 03);
+INSERT INTO Player VALUES (35, 'Lehends', 'Son', 'South Korea', DATE '2023-01-01', 35, 03);
+INSERT INTO Player VALUES (41, '369', 'Bai', 'China', DATE '2023-01-01', 41, 04);
+INSERT INTO Player VALUES (42, 'Kanavi', 'Seo', 'South Korea', DATE '2022-01-01', 42, 04);
+INSERT INTO Player VALUES (43, 'Knight', 'Zhuo', 'China', DATE '2023-01-01', 43, 04);
+INSERT INTO Player VALUES (44, 'Ruler', 'Park', 'South Korea', DATE '2023-01-01', 44, 04);
+INSERT INTO Player VALUES (45, 'Missing', 'Lou', 'China', DATE '2023-01-01', 45, 04);
+INSERT INTO Player VALUES (51, 'Bin', 'Chen', 'China', DATE '2021-01-01', 51, 05);
+INSERT INTO Player VALUES (52, 'Xun', 'Peng', 'China', DATE '2023-01-01', 52, 05);
+INSERT INTO Player VALUES (53, 'Yagao', 'Zeng', 'China', DATE '2022-01-01', 53, 05);
+INSERT INTO Player VALUES (54, 'Elk', 'Zhao', 'China', DATE '2023-02-01', 54, 05);
+INSERT INTO Player VALUES (55, 'On', 'Luo', 'China', DATE '2023-02-02', 55, 05);
+INSERT INTO Player VALUES (61, 'Zika', 'Tang', 'China', DATE '2023-01-01', 61, 06);
+INSERT INTO Player VALUES (62, 'Tarzan', 'Lee', 'South Korea', DATE '2023-01-01', 62, 06);
+INSERT INTO Player VALUES (63, 'Scout', 'Lee', 'South Korea', DATE '2023-01-01', 63, 06);
+INSERT INTO Player VALUES (64, 'LP', 'Li', 'China', DATE '2022-01-01', 64, 06);
+INSERT INTO Player VALUES (65, 'Hang', 'Fu', 'China', DATE '2023-01-01', 65, 06);
+INSERT INTO Player VALUES (71, 'TheShy', 'Kang', 'South Korea', DATE '2022-01-01', 71, 07);
+INSERT INTO Player VALUES (72, 'Karsa', 'Hung', 'PCS', DATE '2023-01-01', 72, 07);
+INSERT INTO Player VALUES (73, 'Xiaohu', 'Li', 'China', DATE '2023-01-01', 73, 07);
+INSERT INTO Player VALUES (74, 'Light', 'Wang', 'China', DATE '2023-01-01', 74, 07);
+INSERT INTO Player VALUES (75, 'Crisp', 'Liu', 'China', DATE '2020-01-01', 75, 07);
+INSERT INTO Player VALUES (81, 'Dhokla', 'Doshi', 'USA', DATE '2023-04-06', 81, 08);
+INSERT INTO Player VALUES (82, 'Contractz', 'Garcia', 'USA', DATE '2023-04-06', 82, 08);
+INSERT INTO Player VALUES (83, 'Palafox', 'Palafox', 'USA', DATE '2023-04-06', 83, 08);
+INSERT INTO Player VALUES (84, 'FBI', 'Huang', 'Australia', DATE '2023-05-27', 84, 08);
+INSERT INTO Player VALUES (85, 'IgNar', 'Lee', 'South Korea', DATE '2023-05-27', 85, 08);
 
 INSERT INTO Match1 VALUES ('Finals', 5);
 INSERT INTO Match1 VALUES ('Semifinals1', 5);
@@ -307,10 +302,19 @@ INSERT INTO Match1 VALUES ('Quarterfinals3', 5);
 INSERT INTO Match1 VALUES ('Quarterfinals4', 5);
 	 
 INSERT INTO Match2 VALUES (101, '2023 LOL Worlds', DATE '2023-11-02', TO_TIMESTAMP('2023-11-02 17:00:00', 'YYYY-MM-DD HH24:MI:SS'), '3-0', 07, 08, 07, 'Quarterfinals1');
-INSERT INTO Match2 VALUES (102, '2023 LOL Worlds', DATE '2023-11-02', TO_TIMESTAMP('2023-11-02 17:00:00', 'YYYY-MM-DD HH24:MI:SS'), '3-2', 01, 05, 05, 'Quarterfinals2');
-INSERT INTO Match2 VALUES (103, '2023 LOL Worlds', DATE '2023-11-02', TO_TIMESTAMP('2023-11-02 17:00:00', 'YYYY-MM-DD HH24:MI:SS'), '3-1', 03, 04, 04, 'Quarterfinals3');
-INSERT INTO Match2 VALUES (104, '2023 LOL Worlds', DATE '2023-11-02', TO_TIMESTAMP('2023-11-02 17:00:00', 'YYYY-MM-DD HH24:MI:SS'), '3-0', 02, 06, 02, 'Quarterfinals4');
-INSERT INTO Match2 VALUES (201, '2023 LOL Worlds', DATE '2023-11-02', TO_TIMESTAMP('2023-11-02 18:00:00', 'YYYY-MM-DD HH24:MI:SS'), '3-2', 07, 05, 07, 'Semifinals1');
-INSERT INTO Match2 VALUES (202, '2023 LOL Worlds', DATE '2023-11-02', TO_TIMESTAMP('2023-11-02 18:00:00', 'YYYY-MM-DD HH24:MI:SS'), '3-1', 02, 04, 02, 'Semifinals1');
-INSERT INTO Match2 VALUES (301, '2023 LOL Worlds', DATE '2023-11-02', TO_TIMESTAMP('2023-11-02 19:00:00', 'YYYY-MM-DD HH24:MI:SS'), '3-0', 02, 07, 02, 'Finals');
+INSERT INTO Match2 VALUES (102, '2023 LOL Worlds', DATE '2023-11-03', TO_TIMESTAMP('2023-11-02 17:00:00', 'YYYY-MM-DD HH24:MI:SS'), '3-2', 01, 05, 05, 'Quarterfinals2');
+INSERT INTO Match2 VALUES (103, '2023 LOL Worlds', DATE '2023-11-04', TO_TIMESTAMP('2023-11-02 17:00:00', 'YYYY-MM-DD HH24:MI:SS'), '3-1', 03, 04, 04, 'Quarterfinals3');
+INSERT INTO Match2 VALUES (104, '2023 LOL Worlds', DATE '2023-11-05', TO_TIMESTAMP('2023-11-02 17:00:00', 'YYYY-MM-DD HH24:MI:SS'), '3-0', 02, 06, 02, 'Quarterfinals4');
+INSERT INTO Match2 VALUES (201, '2023 LOL Worlds', DATE '2023-11-11', TO_TIMESTAMP('2023-11-02 18:00:00', 'YYYY-MM-DD HH24:MI:SS'), '3-2', 07, 05, 07, 'Semifinals1');
+INSERT INTO Match2 VALUES (202, '2023 LOL Worlds', DATE '2023-11-12', TO_TIMESTAMP('2023-11-02 18:00:00', 'YYYY-MM-DD HH24:MI:SS'), '3-1', 02, 04, 02, 'Semifinals1');
+INSERT INTO Match2 VALUES (301, '2023 LOL Worlds', DATE '2023-11-19', TO_TIMESTAMP('2023-11-02 19:00:00', 'YYYY-MM-DD HH24:MI:SS'), '3-0', 02, 07, 02, 'Finals');
 
+
+--Inserts for division
+--Broadcaster that Broadcasted all Tournaments
+INSERT INTO Broadcasts VALUES ('TournamentA', DATE '2012-04-08', 101);
+INSERT INTO Broadcasts VALUES ('TournamentB', DATE '2020-12-07', 101);
+INSERT INTO Broadcasts VALUES ('TournamentC', DATE '2016-11-03', 101);
+INSERT INTO Broadcasts VALUES ('LOL', DATE '2018-11-05', 101);
+INSERT INTO Broadcasts VALUES ('2018 Winter Olympics', DATE '2018-02-09', 101);
+INSERT INTO Broadcasts VALUES ('2023 LOL Worlds', DATE '2023-11-02', 101);
